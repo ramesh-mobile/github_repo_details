47e676ef91a0c67f55a2c50092aa972c349a77d2
ramesh <ramesh.c@implevisiontech.com>
2021-07-23 16:40:35 +0530

    updated time for sync

--- app/src/main/java/com/example/githubrepodetailsapp/utils/Constants.kt
+++ app/src/main/java/com/example/githubrepodetailsapp/utils/Constants.kt
@@ -8,6 +8,6 @@ object Constants {
     const val TAG = "Constants"
     const val ITEM_DETAILS = "item_details"
     const val DATA_UPDATED = "data_updated"
-    const val TIME_TO_SYNC : Long = 1000 * 6
+    const val TIME_TO_SYNC : Long = 1000 * 60 * 15
 
 }
