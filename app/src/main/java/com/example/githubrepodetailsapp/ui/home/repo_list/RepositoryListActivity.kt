b8046f15a1740e49b6d61162698ebb4d84f1ed87
ramesh <ramesh.c@implevisiontech.com>
2021-07-24 09:30:51 +0530

    Get service running status

--- app/src/main/java/com/example/githubrepodetailsapp/ui/home/repo_list/RepositoryListActivity.kt
+++ app/src/main/java/com/example/githubrepodetailsapp/ui/home/repo_list/RepositoryListActivity.kt
@@ -1,5 +1,6 @@
 package com.example.githubrepodetailsapp.ui.home.repo_list
 
+import android.app.ActivityManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -50,9 +51,11 @@ class RepositoryListActivity : AppCompatActivity() ,RepositoryListListener,Repos
 
         viewModel.getDatabaseRepository()
 
-        val serviceIntent = Intent(this, FetchRepoService::class.java)
-        serviceIntent.putExtra("inputExtra", "passing any text")
-        ContextCompat.startForegroundService(this.applicationContext, serviceIntent)
+        if(!isMyServiceRunning(FetchRepoService::class.java)) {
+            val serviceIntent = Intent(this, FetchRepoService::class.java)
+            serviceIntent.putExtra("inputExtra", "passing any text")
+            ContextCompat.startForegroundService(this.applicationContext, serviceIntent)
+        }
     }
 
     fun registerReciever(){
@@ -60,6 +63,14 @@ class RepositoryListActivity : AppCompatActivity() ,RepositoryListListener,Repos
         mIntentFilterGeofence?.addAction(mBroadcastGeofenceAction)
         registerReceiver(mReceiverGeofence, mIntentFilterGeofence)
 
+    }
+
+    fun isMyServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            return (serviceClass.name == service.service.className)
+        }
+        return false
     }
 
     override fun onDestroy() {
@@ -73,25 +84,25 @@ class RepositoryListActivity : AppCompatActivity() ,RepositoryListListener,Repos
 
     var mReceiverGeofence: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            var isDataUpdated = intent.getBooleanExtra(Constants.DATA_UPDATED,false)
+            var isDataUpdated = intent.getBooleanExtra(Constants.DATA_UPDATED, false)
             if(isDataUpdated){
                 Toast.makeText(context, "data is updated", Toast.LENGTH_LONG).show()
                 viewModel.getDatabaseRepository()
-                print(TAG,"Data is updated")
+                print(TAG, "Data is updated")
             }
         }
     }
 
     private fun initialzeRecycler() {
-        repositoryAdapter = RepositoryAdapter(repoList as List<ItemModel?>, this,this)
+        repositoryAdapter = RepositoryAdapter(repoList as List<ItemModel?>, this, this)
         recycler_view.setHasFixedSize(true)
         recycler_view.setItemViewCacheSize(20)
         recycler_view.setDrawingCacheEnabled(true)
         recycler_view.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH)
         recycler_view.setItemAnimator(DefaultItemAnimator())
         recycler_view.addItemDecoration(
-            DividerItemDecoration(this,
-                DividerItemDecoration.VERTICAL)
+                DividerItemDecoration(this,
+                        DividerItemDecoration.VERTICAL)
         )
         recycler_view.setLayoutManager(GridLayoutManager(this, 1))
         recycler_view.setAdapter(repositoryAdapter)
@@ -109,8 +120,8 @@ class RepositoryListActivity : AppCompatActivity() ,RepositoryListListener,Repos
     }
 
     override fun onItemClicked(itemModel: ItemModel?) {
-        var intent = Intent(this,RepoDetailsActivity::class.java)
-        intent.putExtra(Constants.ITEM_DETAILS,itemModel)
+        var intent = Intent(this, RepoDetailsActivity::class.java)
+        intent.putExtra(Constants.ITEM_DETAILS, itemModel)
         startActivity(intent)
     }
 }
