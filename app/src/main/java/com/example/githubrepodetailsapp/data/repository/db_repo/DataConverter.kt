9e22346d02f76fb6188fdb4bd0278f7e603d4e63
ramesh <ramesh.c@implevisiontech.com>
2021-07-27 20:35:12 +0530

    remove list to string function

--- app/src/main/java/com/example/githubrepodetailsapp/data/repository/db_repo/DataConverter.kt
+++ app/src/main/java/com/example/githubrepodetailsapp/data/repository/db_repo/DataConverter.kt
@@ -48,15 +48,6 @@ class DataConverter : Parcelable{
     }
 
     @TypeConverter
-    fun itemModelListToString(itemModels : List<ItemModel?>?) : String?{
-        if(itemModels==null) return null
-        val type: Type = object : TypeToken<List<ItemModel>>() {}.type
-        val jsonString : String = gson.toJson(itemModels, type)
-        return jsonString
-    }
-
-
-    @TypeConverter
     fun stringToItemModelList(data: String?): List<ItemModel?>? {
         if (data == null) {
             return emptyList<ItemModel>()
